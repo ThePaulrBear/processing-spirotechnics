bdbbe7ddfea133a24d04ea4a5845b2bc9fcb7a6a
Paul Wintz <Paul.Wintz@Arthrex.com>
2021-04-07 23:57:42 -0700

    Remove FrameTransitioner string

--- src/paul/wintz/processingspirotechnics/EnglishStrings.java
+++ src/paul/wintz/processingspirotechnics/EnglishStrings.java
@@ -3,7 +3,6 @@ package paul.wintz.processingspirotechnics;
 import paul.wintz.stringids.StringIdMap;
 import paul.wintz.stringids.StringIds;
 
-import static paul.wintz.stringids.StringIds.FRAME_TRANISITIONER_CHANGED;
 import static paul.wintz.stringids.StringIds.*;
 
 class EnglishStrings {
@@ -11,7 +10,6 @@ class EnglishStrings {
             .setRequiredIds(StringIds.values())
             .putStringId(IS_RECORDING_CHANGED, "Is recording? %b")
             .putStringId(FPS_CHANGED_TO, "FPS set to %.1f")
-            .putStringId(FRAME_TRANISITIONER_CHANGED, "Frame transitioner changed to %s")
             .putStringId(IMAGE_SAVED_TO, "Image saved to\n%s")
             .putStringId(FAILED_IMAGE_SAVE, "Image failed to save to\n%s")
             .putStringId(SEGMENT_COUNT_CHANGED_TO, "Segment count changed to %d")
